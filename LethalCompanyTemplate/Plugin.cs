994020af9b870015c5fa4d662a3b530809ff6737
Paul Merkamp <pmerkamp@butler.edu>
2023-11-22 00:08:57 -0500

    initial commit

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -1,14 +1,31 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Diagnostics;
 
-namespace LethalCompanyTemplate
+namespace LCNoScannerPingSound
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private void Awake()
+        private Harmony harmony;
+
+        public void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            harmony = new Harmony("LCNoScannerPingSound");
+            harmony.PatchAll();
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(HUDManager), "Awake")]
+        public class HUDManager_Awake_Patch
+        {
+            static void Postfix(HUDManager __instance)
+            {
+                __instance.scanSFX = null;
+            }
         }
     }
 }
---
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LCNoScannerPingSound
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LCNoScannerPingSound";
+        public const string PLUGIN_NAME = "LCNoScannerPingSound";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
